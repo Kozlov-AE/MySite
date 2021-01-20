d420f68a88f40b2d9308f1957cdc8eadcbae322f
Kozlov-AE <allek-007@list.ru>
2021-01-20 16:04:56 +0300

    FirstCommit Создан шаблон сайта, выбран макет и редактор контента.

---
+++ kozlov-ae.ru/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kozlov_ae.ru.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
